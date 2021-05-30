0e9403eb158b5e184e3ba0b0a0de92c9ccdf26bb
Berzaf <berzafastroo2012@gmail.com>
2021-05-29 23:27:27 -0500

    db_connection

---
+++ source/db_connect.php
@@ -0,0 +1,17 @@
+<?php
+
+$username = 'root';
+$password = '';
+$dsn = 'mysql:host=localhost; dbname=mydb';
+
+try {
+    $conn = new PDO($dsn, $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
