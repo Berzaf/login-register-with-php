cbec26769c32d68f38c8849f5e4920b3baf81789
Berzaf <berzafastroo2012@gmail.com>
2021-05-29 23:43:39 -0500

    add register

---
+++ register.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once 'source/db_connect.php';
+
+if(isset($_POST['register-btn'])) {
+
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    
+
+    try {
+    $sql = "INSERT INTO users (username, password, email, to_date)
+                VALUES (:username, :password, :email, now())";
+
+    $statement = $conn->prepare($sql);
+    $statement->execute(array(':username' => $username, ':password' => $hashed_password, ':email' => $email));
+
+    if($statement->rowCount() == 1) {
+    header('location: index.html');
+    }
+    }
+    catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+    }
+}
+
+?>
