c40856acd731599cef75fd8b910cf12bbd89ab13
Berzaf <berzafastroo2012@gmail.com>
2021-06-01 22:30:16 -0500

    Add file

--- db_connect.php
+++ db_connect.php
@@ -1,19 +1,15 @@
 <?php
 
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$username = 'pma';
+$password = '';
+$dsn = 'mysql:host=localhost; dbname=mydb';
 
-?> 
+$conn = new PDO($dsn, $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
+
+?>
 
 
 
